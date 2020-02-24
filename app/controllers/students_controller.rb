f71833147bb519faf9e89c6a242efe5f56306bf2
SteRobWms <stephen.r.williams.87@gmail.com>
2020-02-23 22:43:04 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,13 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+    # binding.pry
+    @student = current_student
+  end
+
+  def current_student
+    Student.find(params[:id])
+  end
 end
